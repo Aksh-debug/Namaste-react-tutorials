c4d71a8528b67ad334ff1ed042aadfaabd4d8d1a
Aksh-debug <71635101+Aksh-debug@users.noreply.github.com>
2023-09-17 11:20:02 +0530

    Finding the Path

---
+++ Episode-07/README.md
@@ -0,0 +1,28 @@
+# React-Tutorials
+# Episode 01
+# Episode 02
+# Episode 03
+# Episode 04 
+# Episode 05
+# Episode 06
+# Episode 07
+
+# Food Ordering structure
+
+/*
+ * Header
+   - Logo
+   - Nav items
+ * Body
+   - Search
+   - Restaurant Container
+     - restaurant card
+ * Footer
+   - Copyright
+   - Links
+   - Address
+   - Contact
+
+*/
+
+
---
+++ Episode-07/src/components/Shimmer.js
@@ -0,0 +1,64 @@
+const Shimmer = () => {
+    return (
+        <>
+            <div className="shimmer-search-container">
+                <div className="shimmer-search"></div>
+            </div>
+            <div className="shimmer-container">
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card"></div>
+                    <div className="shimmer-detail1"></div>
+                    <div className="shimmer-detail2"></div>
+                </div>
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card"></div>
+                    <div className="shimmer-detail1"></div>
+                    <div className="shimmer-detail2"></div>
+                </div>
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card"></div>
+                    <div className="shimmer-detail1"></div>
+                    <div className="shimmer-detail2"></div>
+                </div>
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card"></div>
+                    <div className="shimmer-detail1"></div>
+                    <div className="shimmer-detail2"></div>
+                </div>
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card"></div>
+                    <div className="shimmer-detail1"></div>
+                    <div className="shimmer-detail2"></div>
+                </div>
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card"></div>
+                    <div className="shimmer-detail1"></div>
+                    <div className="shimmer-detail2"></div>
+                </div>
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card"></div>
+                    <div className="shimmer-detail1"></div>
+                    <div className="shimmer-detail2"></div>
+                </div>
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card"></div>
+                    <div className="shimmer-detail1"></div>
+                    <div className="shimmer-detail2"></div>
+                </div>
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card"></div>
+                    <div className="shimmer-detail1"></div>
+                    <div className="shimmer-detail2"></div>
+                </div>
+                <div className="shimmer-card-container">
+                    <div className="shimmer-card"></div>
+                    <div className="shimmer-detail1"></div>
+                    <div className="shimmer-detail2"></div>
+                </div>
+            </div>
+
+        </>
+    )
+};
+
+export default Shimmer;
